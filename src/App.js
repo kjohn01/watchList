614e06f65dafb8e9c85a40fce90fed41fcde551b
KJohn <mayacafe123@gmail.com>
2020-02-04 22:20:03 +0800

    add bootsrtap, router and NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { NavBar } from "./components/NavBar";
+import { Products } from "./components/Products";
+import { Cart } from "./components/Cart";
 import './App.css';
 
-function App() {
+const App = props => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/">
+            <Products />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+
+const NavBar = props => {
+  return (
+    <Nav defaultActiveKey="/" as="ul">
+        <Nav.Item as="li">
+            <Nav.Link href="/">Products</Nav.Link>
+        </Nav.Item>
+        <Nav.Item as="li">
+            <Nav.Link eventKey="cart" href="/cart">Cart</Nav.Link>
+        </Nav.Item>
+    </Nav>
+  );
+}
+
+export default NavBar;
