e8162f2c3eeeb3b1e263fe6fe024dba336b9cf7f
KJohn <mayacafe123@gmail.com>
2020-02-05 01:46:24 +0800

    Redirect empty cart

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -4,11 +4,18 @@ import ShopContext from '../context/shop-context';
 
 const Cart = props => {
     const context = useContext(ShopContext);
+    if (context.cart.length > 0) {
+        let cartItems = context.cart.map((item, i) => <CartItem key={i} id={item.id} title ={item.title} quantity={item.quantity} />);
 
-    let cartItems = context.cart.map((item, i) => <CartItem key={i} id={item.id} title ={item.title} quantity={item.quantity} />);
-
-    return(
-        <div className="d-flex justify-content-center">{cartItems}</div>
+        return(
+            <div className="d-flex justify-content-center">{cartItems}</div>
+        );
+    }
+    else return(
+        <div className="d-flex justify-content-center">
+            <h2>Empty Cart</h2>
+            <a className="btn" href="/">Go Shop</a>
+        </div>
     );
 };
 
